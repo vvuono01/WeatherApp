32cee81da277f7c06d97fd28ee3b6074b24b5b5b
Vince Vuono <vvuono01@gmail.com>
2018-05-16 22:36:41 -0400

    Made the button disabled until the user has entered a 5 digit zip code

--- app/src/main/java/com/vvuono/weatherapp/SearchActivity.java
+++ app/src/main/java/com/vvuono/weatherapp/SearchActivity.java
@@ -1,13 +1,43 @@
 package com.vvuono.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 public class SearchActivity extends AppCompatActivity {
+    @BindView(R.id.et_zip_code) EditText zipCodeField;
+    @BindView(R.id.b_search) Button searchButton;
+
+    // For the purposes of this exercise, we will not consider ZIP+4 Codes
+    private final int ZIP_CODE_LENGTH = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+    }
+
+    @OnTextChanged(R.id.et_zip_code)
+    public void onZipCodeChanged(Editable editable) {
+        String zipCode = editable.toString();
+        searchButton.setEnabled(isZipCodeValid(zipCode));
+    }
+
+    @OnClick(R.id.b_search)
+    public void searchForZipCode(View view) {
+         // TODO: Call API
+    }
+
+    private boolean isZipCodeValid(String zipCode) {
+        return zipCode.length() == ZIP_CODE_LENGTH;
     }
 }
