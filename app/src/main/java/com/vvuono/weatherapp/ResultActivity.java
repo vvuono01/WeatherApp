69f1d190f2bbed9ef633d4c73ef34339ec884942
Vince Vuono <vvuono01@gmail.com>
2018-05-17 01:28:53 -0400

    Imported the views into the result activity so the values can be set

--- app/src/main/java/com/vvuono/weatherapp/ResultActivity.java
+++ app/src/main/java/com/vvuono/weatherapp/ResultActivity.java
@@ -3,20 +3,37 @@ package com.vvuono.weatherapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.vvuono.weatherapp.data.OpenWeatherData;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ResultActivity extends AppCompatActivity {
+    @BindView(R.id.tv_weather) TextView weatherText;
+    @BindView(R.id.tv_description) TextView descriptionText;
+    @BindView(R.id.tv_temp) TextView tempText;
+    @BindView(R.id.tv_wind_speed) TextView windSpeedText;
+    @BindView(R.id.tv_pressure) TextView pressureText;
+    @BindView(R.id.tv_humdity) TextView humidityText;
+    @BindView(R.id.tv_sunrise) TextView sunriseText;
+    @BindView(R.id.tv_location) TextView locationText;
+
+    private OpenWeatherData weatherData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String resultStr = intent.getStringExtra(SearchActivity.RESULTS_KEY);
         Gson gson = new Gson();
-        OpenWeatherData result = gson.fromJson(resultStr, OpenWeatherData.class);
+        weatherData = gson.fromJson(resultStr, OpenWeatherData.class);
+
+        System.out.println(weatherData);
     }
 }
